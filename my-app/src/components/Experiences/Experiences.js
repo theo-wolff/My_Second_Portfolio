f2726bdcb1e9c8c4eaed21dd1af00964768f4c1e
Théo Wolff <theo.wolff2407@gmail.com>
2024-08-28 19:52:45 +0200

    continuation

--- my-app/src/components/Experiences/Experiences.js
+++ my-app/src/components/Experiences/Experiences.js
@@ -55,7 +55,7 @@ const Experiences = () => {
                             <h2 className='experiences-item-title'>Ecole des Mines de Saint-Etienne</h2>
                             <div className='experiences-item-title-bar'></div>
                             <div className="experiences-item-text">
-                                <p style={{opacity:0.6, fontWeight: "bold"}}>Since 2022 Gardanne</p>
+                                <p style={{opacity:0.6, fontWeight: "bold"}}>Since 2022, Gardanne, France</p>
                                 <p>Second year of a three-year engineering masters degree in Computer Science and Microelectronics.</p>
                                 <p>I am currently pursuing my studies at an esteemed engineering school, specializing in the fields of computer science and microelectronics, situated in the picturesque southern region of France.</p>
                                 <p>Immersed in an intellectually stimulating environment, I am fortunate to be a part of an institution that fosters innovation and excellence in these cutting-edge disciplines. </p>
@@ -92,7 +92,7 @@ const Experiences = () => {
                             <h2 className='experiences-item-title'>Lycée Kleber</h2>
                             <div className='experiences-item-title-bar'></div>
                             <div className="experiences-item-text">
-                                <p style={{opacity:0.6, fontWeight: "bold"}}>From 2020 to 2022 Strasbourg</p>
+                                <p style={{opacity:0.6, fontWeight: "bold"}}>2020-2022, Strasbourg, France</p>
                                 <p>Preparatory classes: two-year intensive preparation for the national competitive entrance exams to leading French engineering schools</p>
                                 <p>This intensive academic journey has instilled in me a strong sense of discipline and perseverance.</p>
                                 <p>I have developed a robust skill set and a strategic mindset.</p>
@@ -117,7 +117,7 @@ const Experiences = () => {
                             <h2 className='experiences-item-title'>Lycée Notre-Dame</h2>
                             <div className='experiences-item-title-bar'></div>
                             <div className="experiences-item-text">
-                                <p style={{opacity:0.6, fontWeight: "bold"}}>From 2017 to 2020 Strasbourg</p>
+                                <p style={{opacity:0.6, fontWeight: "bold"}}>2017-2020, Strasbourg, France</p>
                                 <p>Scientific Baccalauréat with major in Sciences, obtained, with honours, European Option. Equivalent to GCSE A-levels.</p>
                             </div>
                         </motion.div>)}
@@ -145,7 +145,7 @@ const Experiences = () => {
                                             onClick={()=> onHandleClick('2.1')} 
                                             className='experiences-item'
                                             >
-                                    <motion.h4 layout>ASML: Software Intern</motion.h4>
+                                    <motion.h4 layout>ASML Software Intern</motion.h4>
                             </motion.div>
                             {isOpen=='2.1' && (
                             <motion.div className="experiences-expand" 
@@ -157,16 +157,21 @@ const Experiences = () => {
                                 <h2 className='experiences-item-title'>ASML Software Intern</h2>
                                 <div className='experiences-item-title-bar'></div>
                                 <div className="experiences-item-text">
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-                                    4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
-
+                                    <p style={{opacity:0.6, fontWeight: "bold"}}>April 2024-August 2024 ASML, Veldhoven, The Netherlands </p>
+                                    <p>During my second year at Mines de Saint-Étienne, I had the opportunity to undertake a four- to five-month internship abroad. I chose to complete this internship at ASML (Advanced Semiconductor Materials Lithography), one of the world’s leading technology companies, under the supervision of Mr. Marijn van Donkelaar, a Senior Software Engineer. ASML designs and manufactures advanced lithography machines used by major chipmakers like Intel, Samsung, and TSMC to produce computer chips and integrated circuits.</p>
+                                    <p>The mission that was assigned to me is the PERT modernization and continuation. PERT means Project Evaluation Review Technique. It is a project
+                                    management tool used to schedule, organize and coordinate tasks from a project. </p>
+                                    <p>I had the opportunity to work on a Single Page Application (SPA) based on a JavaScript Framework React for the front-end
+                                    and an ASP.NET Core back-end. This applciation had already been started by a previous intern. My mission was to keep working on a node base application to create PERT diagrams.</p>
+                                    
+                                    <p>My missions mainly consisted of:</p>
+                                    <ul>
+                                        <li>Hosting the application using IIS web server.</li>
+                                        <li>Front end redesign</li>
+                                        <li>Adding a Save and Load functionality</li>
+                                        <li>Interacting with Jira with the aim of developping a two-way system. Creating/Updating tickets from the PERT to Jira and Creating/Updating tickets in Jira to the PERT.</li>
+                                        <li>Adding a multi-user functionality to allow cooperation while working on a same PERT.</li>
+                                    </ul>
                                 </div>
                             </motion.div>)} 
 
@@ -190,7 +195,7 @@ const Experiences = () => {
                             <h2 className='experiences-item-title'>Salesman / Animator</h2>
                             <div className='experiences-item-title-bar'></div>
                             <div className="experiences-item-text">
-                                <p style={{opacity:0.6, fontWeight: "bold"}}>July 2023 Ravensburger Roppenheim</p>
+                                <p style={{opacity:0.6, fontWeight: "bold"}}>July 2023 Ravensburger, Roppenheim, France</p>
                                 <p>I worked for one month as a Salesman Animator at Ravensburger, where my responsibilities included managing inventory, ensuring shelves were consistently replenished, and driving product sales. I also provided personalized customer advice, helping clients choose the right products based on their needs and preferences. My role required strong communication skills and a keen understanding of the products to effectively engage with customers and enhance their shopping experience.</p>
                             </div>
                         </motion.div>)}                        
@@ -225,7 +230,7 @@ const Experiences = () => {
                                 <h2 className='experiences-item-title'>Sidel HMI Intern</h2>
                                 <div className='experiences-item-title-bar'></div>
                                 <div className="experiences-item-text">
-                                    <p style={{opacity:0.6, fontWeight: "bold"}}>January 2023 Strasbourg</p>
+                                    <p style={{opacity:0.6, fontWeight: "bold"}}>January 2023 Sidel, Reichstett, France </p>
                                     <p>As part of my studies at Mines de Saint-Étienne in the ISMIN program (Engineering with a focus on Microelectronics and Computer Science), I had the opportunity to complete a one-month internship. In January, I had the privilege of undertaking this internship at Sidel (Société industrielle des emballages légers) in Reichstett, under the guidance of Mr. LINGER, an HMI engineer.</p>
                                     <p>Sidel is a major player in packaging line engineering, offering equipment and services for the packaging of liquids, food products, cleaning supplies, and personal care items</p>
                                     <p>My three missions were:</p>
--- my-app/src/components/Portfolio/Portfolio.js
+++ my-app/src/components/Portfolio/Portfolio.js
@@ -2,6 +2,12 @@ import React, { act } from 'react'
 import './Portfolio.css'
 import P_Item from './P_Item'
 import foule from '../../img/foule.png'
+import portfolio from '../../img/portfolio.jpg'
+import Predictive from '../../img/predictive.jpg'
+import pcsn from '../../img/pcsn.jpg'
+import opti  from '../../img/opti.jpg'
+import proto  from '../../img/proto.jpg'
+
 import { useState } from 'react';
 // import P_Item_Expand from './P_Item_Expand';
 import { motion } from 'framer-motion';
@@ -39,18 +45,24 @@ const Portfolio = () => {
                 <div onClick={()=>setCurrent('Python')} className='portfolio-category-item' style={{opacity: current=='Python'? 0.5 : 1}}>Python</div>
                 <div onClick={()=>setCurrent('C')} className='portfolio-category-item' style={{opacity: current=='C'? 0.5 : 1}}>C</div>
             </div>
-            {(current=='All') && <div className='portfolio-items'>
-                <P_Item image={foule} languages={['HTML','CSS','JS']} title="jh zfiofa heziafo " text="hfkoezabfhjsdf gdzsha f gd hs fgdshqj fgdshqj fgdshqj fgdshqj fgdsqhjofzeatuyifodshqfj dkqfgchdsqjuo  fheoz afheiza fheuzaio fhezuai fheizu ahfeuzi afhgezuia fhgeuoizagf uie zafgeuizagfueiza gfueiza gfuezai fgeuizaf gjsklf bcnjdsakvgdzahjk"></P_Item>
-                <P_Item image={foule} languages={['HTML']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
-                <P_Item image={foule} languages={['Python']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
-            </div>}
-            {(current=='Web') && <div className='portfolio-items'>
-                <P_Item image={foule} languages={['HTML','CSS','JS']} title="jh zfiofa heziafo " text="hfkoezabfhjsdf gdzsha f gd hs fgdshqj fgdshqj fgdshqj fgdshqj fgdsqhjofzeatuyifodshqfj dkqfgchdsqjuo  fheoz afheiza fheuzaio fhezuai fheizu ahfeuzi afhgezuia fhgeuoizagf uie zafgeuizagfueiza gfueiza gfuezai fgeuizaf gjsklf bcnjdsakvgdzahjk"></P_Item>
-                <P_Item image={foule} languages={['HTML']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
-            </div>}
-            {(current=='Python') && <div className='portfolio-items'>
-                <P_Item image={foule} languages={['Python']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
-            </div>}
+            <div className='portfolio-items'>
+                {(current=='All' || current=='Web') && <P_Item image={portfolio} languages={['HTML','CSS','React']} title="Portfolio" text="I created my first portfolio to familiarize myself with HTML, CSS, and JavaScript. After completing my internship at ASML, I was able to build a second portfolio using React, applying the knowledge and skills I developed during this experience."></P_Item>}
+                {(current=='All' || current=='C') && <P_Item image={Predictive} languages={['C']} title="Prediction text" text="The project focuses on predictive text input. The goal is to enter a series of characters, and the program suggests n words starting with that series from a certain dictionary. The dictionary updates with each search, and users can add, edit, or delete words from it."></P_Item>}
+                {(current=='All' || current=='Python') && <P_Item image={foule} languages={['Python']} title="Supervised self-initiative work" text="Modelling crowd movements during public buildings evacuation. Python modelling of a room evacuation with obstacles and visualisation of the room with a graphical interface (Tkinter)."></P_Item>}
+                {(current=='All' || current=='SystemVerilog') && <P_Item image={pcsn} languages={['SystemVerilog']} title="PCSN" text="PCSN (Projet de conception d'un system numérique in French) stands for digital system design project. In this project, we implemented the ASCON128 cryptographic algorithm using SystemVerilog. This experience deepened my understanding of lightweight encryption and strengthened my skills in hardware design and SystemVerilog. It also improved my problem-solving abilities, particularly in optimizing and verifying cryptographic implementations."></P_Item>}
+                {(current=='All' || current=='C') && <P_Item image={opti} languages={['C']} title="Challenge-Optimisation" text="The optimisation challenge was to place surveillance points in specific locations in order to cover as many targets as possible while avoiding obstacles. This takes place on a 2D grid. The objective was to minimise the number of surveillance points by finding the exact locations."></P_Item>}
+                {(current=='All' || current=='Python') && <P_Item image={proto} languages={['Python']} title="Projet-Proto" text="This application measures soil dryness using a microcontroller and capacitive sensors. The data is transmitted via a serial connection. The application used to display data was designed with Tkinter."></P_Item>}
+                
+            
+            
+            </div>
+            {/* // {(current=='Web') && <div className='portfolio-items'>
+            //     <P_Item image={foule} languages={['HTML','CSS','JS']} title="jh zfiofa heziafo " text="hfkoezabfhjsdf gdzsha f gd hs fgdshqj fgdshqj fgdshqj fgdshqj fgdsqhjofzeatuyifodshqfj dkqfgchdsqjuo  fheoz afheiza fheuzaio fhezuai fheizu ahfeuzi afhgezuia fhgeuoizagf uie zafgeuizagfueiza gfueiza gfuezai fgeuizaf gjsklf bcnjdsakvgdzahjk"></P_Item>
+            //     <P_Item image={foule} languages={['HTML']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
+            // </div>}
+            // {(current=='Python') && <div className='portfolio-items'>
+            //     <P_Item image={foule} languages={['Python']} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
+            // </div>} */}
             {/* {(current=='Web') && <div className='portfolio-items'>
                 <P_Item image={foule} title="jh zfiofa heziafo " text="hfkoeza fheoz afheiza fheuzaio fhezuai fheizu ahfeuzi afhgezuia fhgeuoizagf uie zafgeuizagfueiza gfueiza gfuezai fgeuizaf gjsklf bcnjdsakvgdzahjk"></P_Item>
                 <P_Item image={foule} title="Melisml" text="hfjk fhezjkaf hezaf hezafio hezaoif ehzauif ehzaf ghezajhfgzah fgyuia fgezyua fgyuezai fgezayufi egazyuf iegzayufi gezayui"></P_Item>
